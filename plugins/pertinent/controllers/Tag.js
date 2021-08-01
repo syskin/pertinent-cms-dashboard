b3679101271a8a57fe2291a238723506c7d670c2
syskin <mcadu@yahoo.fr>
2021-08-01 17:31:22 +0200

    Add tags route

---
+++ plugins/pertinent/controllers/Tag.js
@@ -0,0 +1,69 @@
+"use strict";
+/**
+ * tag.js controller
+ *
+ * @description: A set of functions called "actions" of the `tags` plugin.
+ */
+
+module.exports = {
+    /**
+     * Create a tag.
+     *
+     * @return {Object}
+     */
+    create: async (ctx) => {
+        try {
+        const data = ctx.request.body;
+
+        const tagData = await strapi
+            .query(`tag`, `pertinent`)
+            .create(data);
+
+        ctx.send({ message: `Tag created successfully`, tag: tagData });
+        } catch (e) {
+        return ctx.badRequest(`An error occured`);
+        }
+    },
+
+    /**
+     * Update tag by id.
+     *
+     * @return {Object}
+     */
+    update: async (ctx) => {
+        try {
+        const params = ctx.params;
+        const body = ctx.request.body;
+        const result = await strapi
+            .query(`tag`, `pertinent`)
+            .findOne({ id: params.id });
+
+        if (!result) return ctx.badRequest(`This tag does not exists`);
+
+        const updateData = await strapi
+            .query(`tag`, `pertinent`)
+            .update(params, body);
+
+        ctx.send({ message: `Tag data updated`, tag: updateData });
+        } catch (e) {
+        return ctx.badRequest(`An error occured`);
+        }
+    },
+
+    /**
+     * Delete tag by id.
+     *
+     * @return {Object}
+     */
+    delete: async (ctx) => {
+        try {
+        const params = ctx.params;
+
+        await strapi.query(`tag`, `pertinent`).delete({ id: params.id });
+
+        ctx.send({ message: `Tag deleted successfully` });
+        } catch (e) {
+        return ctx.badRequest(`An error occured`);
+        }
+    },
+};
